a40813d55119f62f6dc17ba5d91f22090367e8bf
jnanendra <jnanendra@gmail.com>
2026-04-28 11:24:37 +0530

    Add gunicorn and whitenoise for deployment

--- satellite_segmentation/settings.py
+++ satellite_segmentation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*qq!uoly9wlq9%s(c2+07q0$y*pulk&!m&#3c2of69ry(kk&(2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
